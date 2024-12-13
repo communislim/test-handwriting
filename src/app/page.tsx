a0423b88573d326870dd9973d732e85847792fa1
communislim <standwally@gmail.com>
2024-12-13 13:31:15 +0900

    Update: 리랜더링 이슈

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 'use client';
-import { useState } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import HandWritingPad from '../components/hand-writing-pad';
 import MathRenderer from '@/components/math-renderer';
 
 export default function Home () {
   const [latex, setLatex] = useState('')
-  const handleConvert = (latex: string) => {
+  const handleConvert = useCallback((latex: string) => {
     setLatex(latex)
-  }
+  }, []);
+
+  useEffect(() => {
+    console.log('컴포넌트가 리렌더링된 시점:', new Date().getTime());
+  }, [latex]);
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-between">
